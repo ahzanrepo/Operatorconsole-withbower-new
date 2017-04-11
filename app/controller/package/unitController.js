273dd96e5913d5162c5535e092c801deb447d7b7
heshanindika <k.heshan.indika@gmail.com>
2017-04-11 17:08:57 +0530

    init unit management application

---
+++ app/controller/package/unitController.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Heshan.i on 4/11/2017.
+ */
+
+(function () {
+
+    opConsoleApp.controller('unitController', function ($scope, ngNotify, $anchorScroll, userService) {
+
+    });
+
+}());
