9db456a623a664ecdf526571608d967560a76399
heshanindika <k.heshan.indika@gmail.com>
2017-04-17 11:51:40 +0530

    init company creation application

---
+++ app/controller/company/createCompanyController.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Heshan.i on 4/17/2017.
+ */
+
+(function () {
+
+    opConsoleApp.controller('createCompanyController', function ($scope, ngNotify, $anchorScroll, userService) {
+
+        $anchorScroll();
+
+
+    });
+
+}());
