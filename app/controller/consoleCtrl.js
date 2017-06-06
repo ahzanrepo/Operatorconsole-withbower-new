c900415ce66105f3ad6fab396d6dab93d8e1fd2e
waruna <waruna@duosoftware.com>
2017-06-06 12:20:43 +0530

    Operator console should have notification facility so notifications such as billing notifications

--- app/controller/consoleCtrl.js
+++ app/controller/consoleCtrl.js
@@ -3,10 +3,20 @@
  */
 
 'use strict';
-opConsoleApp.controller('consoleCtrl', function ($scope,
+opConsoleApp.controller('consoleCtrl', function ($scope, $filter, $state,
                                                  userProfileServices,
                                                  loginService,
-                                                 $state) {
+                                                 subscribeServices) {
+
+    $scope.showAlert = function (tittle, type, msg) {
+        new PNotify({
+            title: tittle,
+            text: msg,
+            type: type,
+            styling: 'bootstrap3',
+            icon: false
+        });
+    };
 
     //get current login user
     //02.get current profile data
@@ -63,4 +73,105 @@ opConsoleApp.controller('consoleCtrl', function ($scope,
             $state.go('sign-in');
         });
     };
+
+    //-------------------- Notifications ------------------------ \\
+    $scope.unredNotifications = 0;
+    $scope.notifications = [];
+    $scope.OnMessage = function (data) {
+
+        if (data.From) {
+            data.avatar = "assets/img/avatar/profileAvatar.png";
+            if($scope.users && $scope.users.length){
+                var sender = $filter('filter')($scope.users, {username: data.From})[0];
+                console.log("Sender ", sender);
+
+                if (sender.avatar) {
+                    data.avatar = sender.avatar;
+                }
+            }
+
+            data.resv_time = new Date();
+            data.read = false;
+
+
+            $scope.$apply(function () {
+                $scope.notifications.push(data);
+                $scope.unredNotifications = $scope.notifications.length;
+            });
+        }
+    };
+
+    subscribeServices.SubscribeEvents(function (event, data) {
+        switch (event) {
+
+            /*case 'agent_connected':
+
+             $scope.agentConnected(data);
+
+             break;
+
+             case 'agent_disconnected':
+
+             $scope.agentDisconnected(data);
+
+             break;
+
+             case 'agent_found':
+
+             $scope.agentFound(data);
+
+             break;
+
+             case 'agent_rejected':
+             $scope.agentRejected(data);
+             break;
+
+             case 'todo_reminder':
+
+             $scope.todoRemind(data);
+
+             break;
+
+             case 'notice':
+
+             $scope.OnMessage(data);
+
+             break;
+             */
+            case 'notice_message':
+
+                $scope.OnMessage(data);
+
+                break;
+        }
+    });
+
+    $scope.isSocketRegistered = false;
+    $scope.veeryNotification = function () {
+
+        subscribeServices.connectSubscribeServer(function (isConnected) {
+
+            if (isConnected) {
+                $scope.isSocketRegistered = true;
+                /*$('#regNotificationLoading').addClass('display-none').removeClass('display-block');
+                 $('#regNotification').addClass('display-block').removeClass('display-none');*/
+            } else {
+                $scope.isSocketRegistered = false;
+                $scope.showAlert("Registration failed", "error", "Disconnected from notifications, Please re-register")
+            }
+        });
+    };
+
+   $scope.veeryNotification();
+
+    $scope.checkAndRegister = function () {
+
+        if (!$scope.isSocketRegistered) {
+            $('#regNotification').addClass('display-none').removeClass('display-block');
+            $('#regNotificationLoading').addClass('display-block').removeClass('display-none');
+            $scope.isLoadingNotifiReg = true;
+            $scope.veeryNotification();
+        }
+    };
+    //-------------------- Notifications End------------------------ \\
 });
